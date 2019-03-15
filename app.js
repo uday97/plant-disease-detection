45a20a6d07d0035d6d6b2454896fd092c087d502
uday97 <uday.agg97@gmail.com>
2019-03-15 11:37:25 +0530

    image being displayed in result

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var bodyParser = require('body-parser');
 var multer  = require('multer');
 const path = require('path');
 const spawn = require("child_process").spawn;
+const hbs = require('hbs');
 
-const port = process.env.PORT || 8081
+const port = 8081
 
 var app = express();
+
+app.set('view engine','hbs');
+
+app.use(express.static('uploads')); 
 
 app.use(express.static(__dirname+'/public'));
 
@@ -18,7 +23,9 @@ var upload = multer({ dest: '/tmp/'});
 function testing(req, res){
     const pythonPro = spawn('python',["./main_test.py"]);
     pythonPro.stdout.on('data', (data) => {
-        res.send(data.toString());
+        res.render('result.hbs',{
+          data: data.toString()
+        });
     });
 }
 
@@ -26,17 +33,12 @@ app.get('/fileupload.html', function (req, res) {
    res.sendFile( __dirname + "/public/" + "index.html" );
 })
 
-app.post('/file_upload', upload.single('file'), function(req, res, next) {
+app.all('/file_upload', upload.single('file'), function(req, res, next) {
  
     const tempPath = req.file.path;
     const targetPath = path.join(__dirname, "./uploads/image.jpg");
     fs.rename(tempPath, targetPath, function(err) {
       if (err) {
-        console.log(err);
-      }
-    });
-    fs.unlink(targetPath, function(err){
-      if(err){
         console.log(err);
       }
     });
